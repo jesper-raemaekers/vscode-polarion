1a169bea45f86c282754af74e269cc5130c2718e
Jesper Raemaekers <jesper.raemaekers@newayselectronics.com>
2021-01-30 23:23:13 +0100

    initial start to parsing html content of polarion to markdown

--- src/editor.ts
+++ src/editor.ts
@@ -1,6 +1,10 @@
+import { resolve } from 'dns';
 import * as vscode from 'vscode';
 import * as pol from './polarion';
 import * as utils from './utils';
+// import * as cleanhtml from 'clean-html';
+// import * as turndown from 'turndown';
+// import TurndownService = require('turndown');
 
 const decorationType = vscode.window.createTextEditorDecorationType({});
 const open = require('open');
@@ -44,7 +48,8 @@ export async function buildHoverMarkdown(workItem: string): Promise<string[]> {
   if (item !== undefined) {
     hover.push(`${workItem} (${item.type.id}) ***${item.title}***  \nAuthor: ${item.author.id}  \n Status: ${item.status.id}`);
     if (item.description) {
-      hover.push(`${item.description?.content}`);
+
+      hover.push(await getMkDiscription(item.description?.content));
     }
     hover.push(`[Open in Polarion](${url})`);
   }
@@ -53,6 +58,74 @@ export async function buildHoverMarkdown(workItem: string): Promise<string[]> {
   }
   return hover;
 }
+
+async function getMkDiscription(des: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+
+    let html2md = require('html-to-md');
+    let strippedHtml = des.replace(/(\r\n|\n|\r)/gm, "");
+
+    //cleanup html sources
+    var cleaner = require('clean-html');
+    var options = {
+      'add-remove-tags': ['div', 'span', 'br', 'tbody', 'p'],
+      'add-remove-attributes': ['style']
+    };
+    cleaner.clean(strippedHtml, options, function (html: string) {
+      var html2json = require('html2json').html2json;
+      var json2html = require('html2json').json2html;
+      let json: any = html2json(html);
+
+      //TODO: fix tables in children
+      for (let n of json['child']) {
+        if (n.tag === 'table') {
+          n = fixTableHeader(n);
+        }
+      }
+
+
+      html = json2html(json);
+      let md = html2md(html);
+      resolve(md);
+    });
+
+
+
+
+
+    // return md;
+  });
+
+}
+
+function fixTableHeader(json: any): any {
+  let rowCnt = 0;
+  let thead: { node: string, tag: string, child: any[] } = { node: 'element', tag: 'thead', child: [] };
+  let tbody: { node: string, tag: string, child: any[] } = { node: 'element', tag: 'tbody', child: [] };
+
+
+  for (let c of json['child']) {
+    console.log(c);
+    if (c['tag'] === 'tr') {
+      let tr = c;
+      if (rowCnt === 0) {
+
+        thead.child.push(tr);
+      }
+      else {
+        tbody.child.push(tr);
+      }
+      rowCnt++;
+    }
+
+  }
+
+  json['child'] = [];
+  json['child'].push(thead);
+  json['child'].push(tbody);
+  return json;
+}
+
 
 export async function handleOpenPolarion() {
   const editor = vscode.window.activeTextEditor;
